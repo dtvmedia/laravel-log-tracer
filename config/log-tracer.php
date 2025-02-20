b3717dfbe1c654b2be132fb6e204ca88c7bf2f9d
dtvmedia <18654895+dtvmedia@users.noreply.github.com>
2025-02-20 20:33:01 +0000

    Fix styling

--- config/log-tracer.php
+++ config/log-tracer.php
@@ -3,6 +3,6 @@
 // config for dtvmedia/laravel-log-tracer
 return [
 
-    'format' => '[{{class_basename}}::{{method}}] {{message}}'
+    'format' => '[{{class_basename}}::{{method}}] {{message}}',
 
 ];
